fa9a52ab36f507cce19a5293a89266baa011865d
fadhilyusni <42725126+fadhilyusni@users.noreply.github.com>
2022-10-12 15:08:07 +0700

    front end

--- cooking-mama-client/src/main/java/com/cookingmama/cookingmamaclient/CookingMamaClientApplication.java
+++ cooking-mama-client/src/main/java/com/cookingmama/cookingmamaclient/CookingMamaClientApplication.java
@@ -2,12 +2,37 @@ package com.cookingmama.cookingmamaclient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@Controller
 public class CookingMamaClientApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CookingMamaClientApplication.class, args);
     }
+
+    @RequestMapping("/login")
+    public String Login(){
+        return "login";
+    }
+    @GetMapping("/register")
+    public String Register(){
+        return "register";
+    }
+    @GetMapping("/home")
+    public String Home(){
+        return "home";
+    }@GetMapping("/private")
+    public String Private(){
+        return "private";
+    }@GetMapping("/detail")
+    public String DetailRecipe(){
+        return "detailrecipe";
+    }
+
 
 }
