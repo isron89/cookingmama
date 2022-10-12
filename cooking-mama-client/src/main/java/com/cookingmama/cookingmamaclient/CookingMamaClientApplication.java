7e24d53c8de400b8a96a46a32a73893b7b02668b
Hussein Isron <47143174+isron89@users.noreply.github.com>
2022-10-12 13:54:07 +0700

    add cooing mama papa client

---
+++ cooking-mama-client/src/main/java/com/cookingmama/cookingmamaclient/CookingMamaClientApplication.java
@@ -0,0 +1,13 @@
+package com.cookingmama.cookingmamaclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CookingMamaClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CookingMamaClientApplication.class, args);
+    }
+
+}
---
+++ cooking-mama-client/src/test/java/com/cookingmama/cookingmamaclient/CookingMamaClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cookingmama.cookingmamaclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CookingMamaClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
