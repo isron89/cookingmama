c0c05c6420ec69c715a0d9449e15e35e26a4db5a
Hussein Isron <47143174+isron89@users.noreply.github.com>
2022-10-13 15:31:21 +0700

    update backend

--- cooking-mama-backend/src/main/java/com/cookingmama/cookingmamabackend/repository/RecipeRepository.java
+++ cooking-mama-backend/src/main/java/com/cookingmama/cookingmamabackend/repository/RecipeRepository.java
@@ -1,13 +1,10 @@
 package com.cookingmama.cookingmamabackend.repository;
 
-import java.util.List;
-
+import com.cookingmama.cookingmamabackend.model.RecipeModel;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
-import com.cookingmama.cookingmamabackend.model.RecipeModel;
+import java.util.List;
 
 @CrossOrigin(origins = "http://localhost:8081")
 public interface RecipeRepository extends JpaRepository<RecipeModel, Long> {
