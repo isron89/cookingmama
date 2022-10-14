06f7c6257e7b0cc2c5508de44a39e4c9682ae877
fadhilyusni <42725126+fadhilyusni@users.noreply.github.com>
2022-10-14 13:10:16 +0700

    edit folder css

--- cooking-mama-client/src/main/java/com/cookingmama/cookingmamaclient/CookingMamaClientApplication.java
+++ cooking-mama-client/src/main/java/com/cookingmama/cookingmamaclient/CookingMamaClientApplication.java
@@ -23,10 +23,7 @@ public class CookingMamaClientApplication {
     public String Register(){
         return "register";
     }
-    @GetMapping("/home")
-    public String Home(){
-        return "home";
-    }@GetMapping("/private")
+    @GetMapping("/private")
     public String Private(){
         return "private";
     }@GetMapping("/detail")
