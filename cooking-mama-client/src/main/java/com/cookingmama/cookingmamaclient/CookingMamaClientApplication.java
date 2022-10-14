b9fa94730fdbdf8ab8c37171d067400e58ba12d3
Dhimas Chandra Bagaskara <dimascandrabagaskara@gmail.com>
2022-10-14 12:43:25 +0700

    Frontend edit and create page

--- cooking-mama-client/src/main/java/com/cookingmama/cookingmamaclient/CookingMamaClientApplication.java
+++ cooking-mama-client/src/main/java/com/cookingmama/cookingmamaclient/CookingMamaClientApplication.java
@@ -3,6 +3,7 @@ package com.cookingmama.cookingmamaclient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,12 +27,28 @@ public class CookingMamaClientApplication {
     @GetMapping("/home")
     public String Home(){
         return "home";
-    }@GetMapping("/private")
+    }
+    @GetMapping("/private")
     public String Private(){
         return "private";
-    }@GetMapping("/detail")
+    }
+    @GetMapping("/detail")
     public String DetailRecipe(){
         return "detailrecipe";
+    }
+
+    @GetMapping("/create")
+    public String Create(Model model){
+
+        model.addAttribute("author", "username123");
+        return "create";
+    }
+
+    @GetMapping("/edit")
+    public String Edit(Model model){
+
+        model.addAttribute("authorEdit", "Singgih");
+        return "edit";
     }
 
 
