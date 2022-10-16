7653032ed76a09adcaf4ffbc9211a62e2e5a44c6
Dhimas Chandra Bagaskara <dimascandrabagaskara@gmail.com>
2022-10-16 12:05:00 +0700

    Update frontend edit and create html css

--- cooking-mama-client/src/main/java/com/cookingmama/cookingmamaclient/CookingMamaClientApplication.java
+++ cooking-mama-client/src/main/java/com/cookingmama/cookingmamaclient/CookingMamaClientApplication.java
@@ -7,6 +7,11 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.net.CacheRequest;
+    import java.util.HashMap;
+import java.util.Map;
 
 @SpringBootApplication
 @Controller
@@ -42,6 +47,14 @@ public class CookingMamaClientApplication {
     public String Edit(Model model){
 
         model.addAttribute("authorEdit", "Singgih");
+//        CacheRequest object;
+//        String body = object.getBody();
+//        Gson gson = new Gson();
+//        Map<String, Object> map = gson.fromJson(body, HashMap.class);
+//        Map<String, Object> data = (Map<String, Object>) map.get("data");
+//        Map<String, Object> creditTerm = (Map<String, Object>) data.get("MS8B50FHS");
+//        String creditTermValue = creditTerm.get("creditTerm").toString();
+//        toStringSystem.out.println(creditTermValue);
         return "edit";
     }
 
---
+++ cooking-mama-client/src/main/java/com/cookingmama/cookingmamaclient/model/Recipe.java
@@ -0,0 +1,21 @@
+package com.cookingmama.cookingmamaclient.model;
+
+public class Recipe {
+
+    private String id;
+
+    private String nama;
+
+    private String ingredients;
+
+    private String howto;
+
+    private Boolean publik;
+
+    private String userid;
+
+
+    public Recipe(){
+
+    }
+}
