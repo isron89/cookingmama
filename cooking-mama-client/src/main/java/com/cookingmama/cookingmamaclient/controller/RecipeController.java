94a5dcc130127d744bdd6d5c45e20bc6197464d8
fadhilyusni <42725126+fadhilyusni@users.noreply.github.com>
2022-10-16 16:46:36 +0700

    get json

--- cooking-mama-client/src/main/java/com/cookingmama/cookingmamaclient/controller/RecipeController.java
+++ cooking-mama-client/src/main/java/com/cookingmama/cookingmamaclient/controller/RecipeController.java
@@ -1,14 +1,26 @@
 package com.cookingmama.cookingmamaclient.controller;
 
+import com.cookingmama.cookingmamaclient.dto.Recipe;
+import com.cookingmama.cookingmamaclient.service.impl.RecipeService;
+import com.cookingmama.cookingmamaclient.service.impl.RecipeServiceImpl;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
 @Controller
 public class RecipeController {
+    @Autowired
+    private RecipeServiceImpl service;
+    @Autowired
+    private ObjectMapper mapper;
 
     @GetMapping("/home")
     public String home (Model model){
+        model.addAttribute("recipes", service.findAll());
         return "home";
     }
     @GetMapping("/private")
--- cooking-mama-client/src/main/java/com/cookingmama/cookingmamaclient/service/impl/RecipeServiceImpl.java
+++ cooking-mama-client/src/main/java/com/cookingmama/cookingmamaclient/service/impl/RecipeServiceImpl.java
@@ -2,12 +2,24 @@ package com.cookingmama.cookingmamaclient.service.impl;
 
 import com.cookingmama.cookingmamaclient.dto.Recipe;
 import com.cookingmama.cookingmamaclient.service.RestClientService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
-public class RecipeServiceImpl  {
-    List<Recipe> recipes = new ArrayList<>();
+@Service
+public class RecipeServiceImpl {
+    @Value("${resource.recipes}")
+    private String resource;
+    @Autowired
+    private RestTemplate restTemplate;
 
-
+    public List<Recipe> findAll(){
+        return Arrays.stream(restTemplate.getForObject(resource, Recipe[].class)).collect(Collectors.toList());
+    }
 }
